78c0e8590131be2736ee4c0fa4073dd85313da00
Isabella Antunes <bella.antunes2@gmail.com>
2021-05-29 09:29:11 -0300

    Resolução dos exercícios do dia 4

--- FUNDAMENTOS/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia_4/exercicios4.js
+++ FUNDAMENTOS/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia_4/exercicios4.js
@@ -1,3 +1,4 @@
+//Parte I
 let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
@@ -39,3 +40,87 @@ for (let key in info) {
   console.log (`${info[key]} e ${info2[key]}`);
   }
 }
+
+//Parte II
+//Exercício 1
+function checkPalindromo(word) {
+  let palindromo = false;
+  for (let index in word) {
+    if (word[index] === word[word.length - 1 - index]) {
+      palindromo = true;
+    } else {
+      palindromo = false;
+    }
+  }
+  console.log(`A palavra é um palíndromo? ${palindromo}`);
+}
+checkPalindromo('arara');
+
+//Exercício 2
+function findTheHighestNumber(arrayNumbers) {
+  let highestNumber = arrayNumbers[0];
+  let indexHighestNumber = 0;
+  for (let index in arrayNumbers) {
+    if (highestNumber < arrayNumbers[index]) {
+      highestNumber = arrayNumbers[index];
+      indexHighestNumber = index;
+    }
+  }
+  console.log(`O índice do maior número é ${indexHighestNumber}`);
+}
+findTheHighestNumber([2, 3, 6, 7, 10, 1]);
+
+//Exercício 3
+function findTheSmallestNumber(arrayNumbers2) {
+  let smallestNumber = arrayNumbers2[0];
+  let indexSmallestNumbers = 0;
+  for (let index in arrayNumbers2) {
+    if (smallestNumber > arrayNumbers2[index]) {
+      smallestNumber = arrayNumbers2[index];
+      indexSmallestNumbers = index;
+    }
+  }
+  console.log(`O índice do menor número é ${indexSmallestNumbers}`);
+}
+findTheSmallestNumber([2, 4, 6, 7, 10, 0, -3]);
+
+//Exercício 4
+function findTheLonggestName(arrayNames) {
+  let longgestName = arrayNames[0];
+  for (let indexArray = 0; indexArray < arrayNames.length; indexArray += 1) {
+    if (longgestName.length < arrayNames[indexArray].length) {
+      longgestName = arrayNames[indexArray];
+    }
+  }
+  console.log(`O nome com maior número de caracteres é: ${longgestName}.`)
+}
+findTheLonggestName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']);
+
+//Exercício 5
+
+
+//Exercício 6
+function logNumber(number) {
+  let somaNumber = 0;
+  for (let index = 1; index <= number; index += 1) {
+    somaNumber += index; 
+  }
+  console.log(`A soma é: ${somaNumber}`);
+}
+logNumber(5);
+
+//Exercício 7
+function comparingStrings(stringWord, stringEnding) {
+  let comparisonOfStrings = false;
+  let index2 = 0;
+  for (let index = stringWord.length - stringEnding.length; index < stringWord.length; index += 1) {
+    if (stringWord[index] === stringEnding[index2]) {
+      comparisonOfStrings = true;
+    } else {
+      comparisonOfStrings = false;
+    }
+    index2 += 1;
+  }
+  console.log(comparisonOfStrings);
+}
+comparingStrings('trybe', 'be');
