8e7db312c42c39d1685e9e6b0fa77d12c93b66ca
Isabella Antunes <bella.antunes2@gmail.com>
2021-06-22 13:57:34 -0300

    Resolução dos exercíos 1 e 2 da parte 3 do dia 3

--- FUNDAMENTOS/bloco-7-javascript-ES6-e-testes-unitarios/dia_3/part3.js
+++ FUNDAMENTOS/bloco-7-javascript-ES6-e-testes-unitarios/dia_3/part3.js
@@ -1,10 +1,54 @@
-//EXERCÍCIO 1
+// EXERCÍCIO 1
+const greetPeople = (parameter) => {
+  let greeting = [];
 
 
-//EXERCÍCIO 2
+  for (let index in parameter) {
+    greeting.push(`Hello ${parameter[index]}`);
+  }
+  return greeting;
+};
+
+const parameter = ['Irina', 'Ashleigh', 'Elsa'];
+const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
+
+const assert = require('assert');
+
+assert.strictEqual(typeof greetPeople, 'function');
+assert.deepStrictEqual(greetPeople(parameter), result);
+
+// EXERCÍCIO 2
+const removeVowels = (parameter2) => {
+  const characters = parameter2.split('');
+  let results = '';
+  let numVowels = 0;
+
+  for (let index = 0; index < characters.length; index += 1) {
+    if (
+      characters[index] === 'a' ||
+      characters[index] === 'o' ||
+      characters[index] === 'i' ||
+      characters[index] === 'e' ||
+      characters[index] === 'u'
+    ) {
+      numVowels += 1;
+      characters[index] = numVowels;
+    }
+  }
+  for (let index of characters) {
+    results += index;
+  }
+  
+  return results;
+};
+
+const parameter2 = 'Dayane';
+const result2 = 'D1y2n3';
+
+assert.strictEqual(typeof removeVowels, 'function');
+assert.strictEqual(removeVowels(parameter2), result2);
+
+// EXERCÍCIO 3
 
 
-//EXERCÍCIO 3
-
-
-//EXERCÍCIO 4
+// EXERCÍCIO 4
