0d5b4ea7ecdb4a1a9e9d39e7fa15698153b8db0b
Isabella Antunes <bella.antunes2@gmail.com>
2021-07-22 13:58:03 -0300

    Resolução dos exercícios de fixação do dia 1

---
+++ FRONT-END/bloco-11-introducao-a-react/dia_1/exercicios/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import AboutMe from './Component';
+
+class App extends React.Component {
+  render() {
+    return (
+    <div>
+      <AboutMe />
+    </div>
+  );
+}
+}
+
+export default App;
---
+++ FRONT-END/bloco-11-introducao-a-react/dia_1/exercicios/src/Component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutMe extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Isabella Antunes</h1>
+        <p>Tenho 29 anos, sou formada em Engenharia Civil e agora estou em uma transição de carreira.</p>
+      </div>
+    );
+  }
+}
+
+export default AboutMe;
